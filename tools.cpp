1dd8c779a183c7bd74e048ead4ec3793c090c4dc
NovasomIndustries <filippo.visocchi@novasomindustries.com>
2017-02-21 10:53:12 +0100

    Start tools in background

--- tools.cpp
+++ tools.cpp
@@ -17,27 +17,16 @@ QString fileName1, fileName2;
 
 void NOVAembed::on_CodeBlock_pushButton_clicked()
 {
-    system("codeblocks");
+    system("codeblocks &");
 }
 
 void NOVAembed::on_Meld_pushButton_clicked()
 {
-    QFile scriptfile("/tmp/script");
-    if ( ! scriptfile.open(QIODevice::WriteOnly | QIODevice::Text) )
-    {
-        update_status_bar("Unable to create /tmp/script");
-        return;
-    }
-    QTextStream out(&scriptfile);
-    out << QString("#!/bin/sh\n");
-    out << QString("echo 0 > /tmp/result\n");
-    out << QString("meld\n");
-    scriptfile.close();
-    run_script();
+    system("meld &");
 }
 
 void NOVAembed::on_QtCreator_pushButton_clicked()
 {
-    system("qtcreator");
+    system("qtcreator &");
 
 }
